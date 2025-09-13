10c9a24cec93df02f69a143eb0a68c62f2b15c47
Peanut040409 <ethanlombaard09@gmail.com>
2025-09-13 22:47:21 +0200

    Started work on image handling

--- resources/js/image-handler.js
+++ resources/js/image-handler.js
@@ -1,6 +1,6 @@
   const input = document.getElementById('imageInput');
   const preview = document.getElementById('preview');
-
+  const imageLabel = document.getElementById('imageLabel');
   input.addEventListener('change', function() {
     const file = this.files[0];
     if (file) {
@@ -10,6 +10,19 @@
         preview.src = e.target.result; // displays the image
       };
       reader.readAsDataURL(file);
+
+      imageLabel.innerHTML =`
+        Name: ${file.name} <br>
+        Size: ${(file.size/1024/1024).toFixed(2)} mb
+      `;
+
+      if ((file.size/1024/1024) > 5) {
+        alert("The file size is above the reccomended limit.");
+      }
+
+      if (!file.type.startsWith('image/')) {
+        alert("The file type is not an image. Please select an image.");
+      }
 
       // Option B: You can also just create an object URL
       // preview.src = URL.createObjectURL(file);
--- resources/views/admin/images/index/forms/create.blade.php
+++ resources/views/admin/images/index/forms/create.blade.php
@@ -2,12 +2,13 @@
     <div class="flex justify-center">
         <form action="/admin/images" method="POST" class="flex justify-center flex-col">
             @csrf
-            <img src="" alt="" id="preview">
+            <img src="" alt="" id="preview" id="image">
+            <label for="image" id="imageLabel"></label>
             <input type="file" name="image" id="imageInput" class="hidden">
-            <label for="image" class="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">Choose file</label>
+            <label for="imageInput" class="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">Choose file</label>
             <br>
             <button type="submit">Upload</button>
         </form>
-        @vite('resources/js/image-handler.js')
+        @vite('resources/js/image-handler.js')  
     </div>
 </x-admin-layout>
